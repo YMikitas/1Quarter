399aa4681e20f431305e86454f208cf8404e701e
YMikitas <ymikitas@gmail.com>
2022-10-16 23:51:14 +0300

    Add Code

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿string text = Console.ReadLine();
+string[] array = text.Split(new char[]{' ',',',';'}, StringSplitOptions.RemoveEmptyEntries);
+
+var result = from word in array
+    where word.Length <= 3
+    select word;
+if (result.Count() == 0) Console.WriteLine("[]");
+else 
+{
+    foreach (string word in result) 
+    Console.Write(word + "; ");
+}
